ba8940cfb0bddbb79c5f9fb12ad1fcc8349ef101
pannman <30381819+pannman@users.noreply.github.com>
2020-09-29 19:59:59 +0900

    config/apprication.rb code mistake

--- config/application.rb
+++ config/application.rb
@@ -15,12 +15,13 @@ module QuizApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+            view_specs: true,
+            helper_specs: true,
+            controller_specs: true,
+            routing_specs: true
+    end
+
   end
-end
-config.generators do |g|
-  g.test_framework :rspec,
-        view_specs: true,
-        helper_specs: true,
-        controller_specs: true,
-        routing_specs: true
 end
