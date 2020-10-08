368f07471abe685d577cc40be31403bc56f2d78d
pannman <30381819+pannman@users.noreply.github.com>
2020-10-08 15:31:29 +0900

    add signup signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  resources :users, only: [:show]
+  root 'static_pages#home'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions:      'users/sessions',
   }
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
